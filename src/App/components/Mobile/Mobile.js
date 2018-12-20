c0d72b90fe89566d46a58bb2e1730040e28f0ea1
lloan <lloanalas@outlook.com>
2018-12-20 07:41:53 -0800

    mobile menu added, icons added, link actions added

--- src/App/components/Mobile/Mobile.js
+++ src/App/components/Mobile/Mobile.js
@@ -1,14 +1,17 @@
 import React, {Component} from 'react';
 import './Mobile.scss';
+import phoneIcon from './Assets/phone.svg';
+import emailIcon from './Assets/envelope.svg';
+import homeIcon from './Assets/home.svg';
 
 class Mobile extends React.Component {
   render() {
     return (
         <div id="mobileMenu" className="container-fluid">
           <div className="row">
-            <div className="col-4">x</div>
-            <div className="col-4">y</div>
-            <div className="col-4">z</div>
+            <div className="col-4 mobile-menu-icon" onClick={e => { window.location.href = '#home'}}><img src={homeIcon} /></div>
+            <div className="col-4 mobile-menu-icon" onClick={e => { window.location.href = 'mailto:support@ledkyb.com'}}><img src={emailIcon} /></div>
+            <div className="col-4 mobile-menu-icon" onClick={e => { window.location.href = 'tel:9094365446'}}><img src={phoneIcon} /></div>
           </div>
         </div>
     );
