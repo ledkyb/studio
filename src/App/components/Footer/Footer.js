a2f8b75eddee93b9e3b96fc19505f8b523997c5d
Amy Silva <amyambard@icloud.com>
2018-12-21 15:25:12 -0800

    Add footer component

---
+++ src/App/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    componentWillMount(){
+
+    }
+
+    render(){
+        return(
+            <div className="container" style={{background: 'green', height: '50px'}}>
+                <p>Footer</p>
+            </div>
+        )
+    }
+}
+
+
+export default Footer;
