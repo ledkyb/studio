10e13ef38df26de0a0adc4439bc20a789c948cce
Amy Silva <amyambard@icloud.com>
2018-12-21 18:22:29 -0800

    Add social media component

---
+++ src/App/components/SocialMedia/SocialMedia.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+class SocialMedia extends Component {
+    componentWillMount(){
+
+    }
+
+    render(){
+        return(
+          <section id="contact" className="row">
+              <div className="container" style={{background: 'blue', height: '50px'}}>
+
+                  <p>SocialMedia</p>
+
+              </div>
+          </section>
+        )
+    }
+}
+
+
+export default SocialMedia;
