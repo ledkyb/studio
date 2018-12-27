39bc0843eeebf553e8a648271ceea8c1cde84c82
Felipe Fernandez <heyitsfelipe@gmail.com>
2018-12-26 17:59:47 -0800

    Adds more bootstrap css and adds custom css, both targeting responsiveness

--- src/App/components/Form/Form.js
+++ src/App/components/Form/Form.js
@@ -1,71 +1,34 @@
 import React from 'react';
-import Button from '../Button/Buttons';
 import './Form.scss';
 
 
 const Form = () => {
     return (
-        <div className="container bg-white">
+        <div className="form container bg-white py-4">
             <form>
                 <div className="row">
-                    <div className="col-lg-6">
-                        <div className="row form-group border-bottom border-dark">
-                            <label className="col-2 my-0 pt-2 text-dark" for="name">Name</label>
-                            <input className="col-10 form-control border-0" type="text" id="name" />
+                    <div className="col-lg-5 offset-lg-1 pr-lg-5 mb-4">
+                        <div className="row form-group border-bot">
+                            <label className="col-2 col-sm-1 col-lg-2 my-0 p-0 pt-2 text-color" for="name">Name</label>
+                            <input className="col-10 col-lg-9 form-control border-0 text-dark" type="text" id="name" />
                         </div>
-                        <div className="row form-group border-bottom border-dark">
-                            <label className="col-2 my-0 pt-2" for="email">Email</label>
-                            <input className="col-10 form-control border-0" type="text" id="email" />
+                        <div className="row form-group border-bot mb-lg-0 mt-lg-4">
+                            <label className="col-2 col-sm-1 col-lg-2 my-0 p-0 pt-2 text-color" for="email">Email</label>
+                            <input className="col-10 form-control border-0 text-dark" type="text" id="email" />
                         </div>
                     </div>
-                    <div className="col-lg-6">
-                        <div className="row form-group">
-                            <textarea className="col-12 form-control border border-dark rounded-0 h-100"></textarea>
+                    <div className="col-lg-5 pl-lg-5 mb-4">
+                        <div className="row form-group h-100">
+                            <textarea className="col-12 form-control border-all rounded-0 text-dark"></textarea>
                         </div>
                     </div>
                 </div>
                 <div className="row form-group">
-                    <button className="form-control col-3 ml-auto border-dark text-dark rounded-0">send</button>
+                    <button className="form-control col-3 offset-9 col-lg-1 offset-lg-10 border-all text-color rounded-0">send</button>
                 </div>
             </form>
         </div>
     );
 };
-
-// const Form = () => {
-//     return (
-//         <section className="row form">
-//             <div className="container">
-//                 <div className="row">
-//                     <form className="col-12">
-//                         <div className="row">
-//                             <div className="col-5 offset-1">
-//                                 <label>Name</label>
-//                                 <input></input>
-//                                 <hr />
-//                                 <label>Email</label>
-//                                 <input></input>
-//                                 <hr />
-//                             </div>
-//                             <div className="col-6">
-//                                 <div className="text-area">
-//                                     <textarea className="float-right"></textarea>
-//                                 </div>
-//                             </div>
-//                         </div>
-//                         <div className="row">
-//                             <div className="col-2 offset-9">
-//                                 <div className="send-button">
-//                                     <Button className="send-button-style" label="send" />
-//                                 </div>
-//                             </div>
-//                         </div>
-//                     </form>
-//                 </div>
-//             </div>
-//         </section>
-//     )
-// };
-
 
 export default Form;
