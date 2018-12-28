d01b492ae71c2871ae17b39ef6ea385f03b357d3
VelazquezLuis <javiervelazquez113@yahoo.com>
2018-12-27 19:57:43 -0600

    added Blog.scss, moved pictures to src, began working in blog.js

--- src/App/components/Sections/Blog/Blog.js
+++ src/App/components/Sections/Blog/Blog.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import './Blog.scss';
+
+// import code from '../../../../../public/images/blog/post-one.jpg'
 
 class Blog extends Component {
     componentWillMount(){
@@ -7,8 +10,67 @@ class Blog extends Component {
 
     render(){
         return(
-            <div className="container" style={{background: 'magenta', height: '150px'}}>
-                <p>Blog</p>
+            <div id="blog" className="container" >
+
+
+
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+    <div class="row">
+
+<div id="blog2" class="col-7 "> 
+  
+</div>
+<div id="blog1" class="col-5 ">
+  <div id="blog3" class="col-12 ">ss </div>
+  <div id="blog4" class="col-12 "> </div>                  
+</div>
+
+</div>
+    </div>
+    <div class="carousel-item">
+    <div class="row">
+
+<div id="blog2" class="col-7 "> 
+  
+</div>
+<div id="blog1" class="col-5 ">
+  <div id="blog3" class="col-12 "> </div>
+  <div id="blog4" class="col-12 "> </div>                  
+</div>
+
+</div>
+    </div>
+    <div class="carousel-item">
+    <div class="row" alt="First slide">
+
+<div id="blog2" class="col-7 "> 
+  
+</div>
+<div id="blog1" class="col-5 " >
+  <div id="blog3" class="col-12 "> </div>
+  <div id="blog4" class="col-12 "> </div>                  
+</div>
+
+</div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+
+
+
+
+                
             </div>
         )
     }
