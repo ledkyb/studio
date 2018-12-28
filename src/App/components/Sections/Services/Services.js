9861415bb83a12da7d474c503bee5fbfb467db5b
Amber Stumpf <amacbain1@msn.com>
2018-12-27 16:34:28 -0800

    Adds responsiveness to Services typography

--- src/App/components/Sections/Services/Services.js
+++ src/App/components/Sections/Services/Services.js
@@ -13,64 +13,77 @@ class Services extends Component {
               <div className="container bg-white">
                   <div className='row mt-5 mb-5 align-bottom'>
                     <h1 className='d-inline col-xs-4 font-weight-bold'>SERVICES</h1>
-                    <span className='heading-span d-inline-block col-1 weight-600 p-3'>/</span>
+                    <span className='heading-span d-inline-block col-1 weight-600 text-center p-3'>/</span>
                     <span className='d-inline col-4 text-nowrap mb-0 weight-600 pt-3 pl-0'>Studio Services</span>
                   </div>
 
-                  <div>
+                  <div className='container'>
+                    <div className='row'>
+                    <ul className='list-group col-md-6'>
 
-                    <ul className='list-group container'>
+
                       <div className='row'>
                         <span className='d-inline diamond'></span>
-                        <li className='weight-500 d-inline col-10 list-group-item border-0'>
-                        Performance Driven Web Apps</li>
+                        <li className='list-group-item weight-500 d-inline col-10 border-0'>
+                          Performance Driven Web Apps</li>
                       </div>
+
                       <div className='row'>
                         <span className='d-inline diamond'></span>
-                        <li className='weight-500 d-inline col-10 list-group-item border-0'>
+                        <li className='list-group-item weight-500 d-inline col-10 border-0'>
                         Proprietary Tool Development</li>
                       </div>
+
                       <div className='row'>
                         <span className='d-inline diamond'></span>
-                        <li className='weight-500 d-inline col-10 list-group-item border-0'>
+                        <li className='list-group-item weight-500 d-inline col-10 border-0'>
                         Strategic Branding & Rebranding</li>
                       </div>
+
                       <div className='row'>
                         <span className='d-inline diamond'></span>
-                        <li className='weight-500 d-inline col-10 list-group-item border-0'>
+                        <li className='list-group-item weight-500 d-inline col-10 border-0'>
                         Graphic Design</li>
                       </div>
-                      <div className='row pb-1'>
+
+                      <div className='row'>
                         <span className='d-inline diamond'></span>
-                        <li className='weight-500 d-inline col-10 list-group-item border-0'>
+                        <li className='list-group-item weight-500 d-inline col-10 border-0'>
                         On-page Search Engine Optimization</li>
                       </div>
-                      <div className='row'>
-                        <span className='d-inline diamond'></span>
-                        <li className='weight-500 d-inline col-10 list-group-item border-0'>
-                        Interactive and Performant Sites</li>
-                      </div>
-                      <div className='row'>
-                        <span className='d-inline diamond'></span>
-                        <li className='weight-500 d-inline col-10 list-group-item border-0'>
-                        Code Clean-up & Project Refactoring</li>
-                      </div>
-                      <div className='row'>
-                        <span className='d-inline diamond'></span>
-                        <li className='weight-500 d-inline col-10 list-group-item border-0'>
-                        Printing Services</li>
-                      </div>
-                      <div className='row mb-5'>
-                        <span className='d-inline diamond'></span>
-                        <li className='weight-500 d-inline col-10 list-group-item border-0'>
-                        Start-up Starter Kits</li>
-                      </div>
                     </ul>
+
+                    <ul className='list-group col-md-6'>
+
+                      <div className='row'>
+                        <span className='d-inline diamond'></span>
+                        <li className='list-group-item weight-500 d-inline col-10 border-0'>
+                        Interactive and Performant Sites</li>
+                        </div>
+
+                        <div className='row'>
+                          <span className='d-inline diamond'></span>
+                          <li className='list-group-item weight-500 d-inline col-10  border-0'>
+                        Code Clean-up & Project Refactoring</li>
+                        </div>
+                       <div className='row'>
+                         <span className='d-inline diamond'></span>
+                         <li className='list-group-item weight-500 d-inline col-10  border-0'>
+                        Printing Services</li>
+                        </div>
+                       <div className='row'>
+                         <span className='d-inline diamond'></span>
+                         <li className='list-group-item weight-500 d-inline col-10 border-0'>
+                          Start-up Starter Kits</li>
+                        </div>
+                    </ul>
+                    </div>
                   </div>
                   <div>
-                    <img className="cloud-3" src={cloud3} />
-                    <img className="cloud-two" src={cloudTwo} />
-                  </div>
+-                    <img className="cloud-3" src={cloud3} />
+-                    <img className="cloud-two" src={cloudTwo} />
+-                  </div>
+
               </div>
           )
       }
