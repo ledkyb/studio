6ca1945f2a223619d1b368609e55b0551b3090d5
Felipe Fernandez <heyitsfelipe@gmail.com>
2018-12-27 19:29:00 -0800

    Adds reusable Button component to form

--- src/App/components/Form/Form.js
+++ src/App/components/Form/Form.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Form.scss';
-
+import Button from '../Button/Buttons';
 
 const Form = () => {
     return (
@@ -17,14 +17,17 @@ const Form = () => {
                             <input className="col-10 form-control border-0 text-dark" type="text" id="email" />
                         </div>
                     </div>
-                    <div className="col-lg-5 pl-lg-5 mb-4">
+                    <div className="col-lg-5 pl-lg-5 mb-4 pr-lg-2">
                         <div className="row form-group h-100">
                             <textarea className="col-12 form-control border-all rounded-0 text-dark"></textarea>
                         </div>
                     </div>
                 </div>
-                <div className="row form-group">
-                    <button className="form-control col-3 offset-9 col-lg-1 offset-lg-10 border-all text-color rounded-0">send</button>
+                <div className="row form-group pr-lg-5">
+                    <div className="col-3 offset-9 col-lg-1 offset-lg-10">
+                        <Button link="#contact" label="send" />
+                    </div>
+                    {/* <button className="form-control col-3 offset-9 col-lg-1 offset-lg-10 border-all text-color rounded-0">send</button> */}
                 </div>
             </form>
         </div>
