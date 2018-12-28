76a900b97e2300efc93d47e8b5068b48df5d2033
Amy Silva <amyambard@icloud.com>
2018-12-27 18:05:48 -0800

    Add social media logos to social media component

--- src/App/components/SocialMedia/SocialMedia.js
+++ src/App/components/SocialMedia/SocialMedia.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 
+import facebook from './Assets/Social/fb-black.svg';
+import twitter from './Assets/Social/tw-black.svg';
+import github from './Assets/Social/git-black.svg';
+import linkedIn from './Assets/Social/li-black.svg';
+import stackOverflow from './Assets/Social/so-black.svg';
+import instagram from './Assets/Social/in-black.svg';
+
 class SocialMedia extends Component {
     componentWillMount(){
 
@@ -7,10 +14,15 @@ class SocialMedia extends Component {
 
     render(){
         return(
-          <section id="contact" className="row">
+          <section id="social" className="row">
               <div className="container">
 
-                  <p>Social Media</p>
+                  <img className="" src={github} alt="Ledkyb GitHub Account link" />
+                  <img className="" src={linkedIn} alt="Ledkyb LinkedIn Account link" />
+                  <img className="" src={stackOverflow} alt="Ledkyb Stack Overflow Account link" />
+                  <img className="" src={facebook} alt="Ledkyb Facebook Account link" />
+                  <img className="" src={instagram} alt="Ledkyb Instagram Account link" />
+                  <img className="" src={twitter} alt="Ledkyb Twitter Account link" />
 
               </div>
           </section>
