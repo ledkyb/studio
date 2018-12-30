b468e1d748ac532ad06d50bed74a493c3d9454ec
lloan <lloanalas@outlook.com>
2018-12-29 23:25:13 -0800

    rebuilt nav - proper usage of bootstrap

--- src/App/components/Nav/Nav.js
+++ src/App/components/Nav/Nav.js
@@ -15,53 +15,59 @@ class Nav extends Component {
 
   render() {
     return (
-        <div className="container p-0">
-          <div className="col-12 p-0">
-            <nav className="navbar navbar-expand-lg navbar-light">
+        <div className="container-fluid nav">
+          <nav className="container">
+            <div className="row">
 
-              <a className="navbar-brand" href="#">
-                <Logo/>
-              </a>
 
-              <button className="navbar-toggler"
-                      type="button"
-                      data-toggle="collapse"
-                      data-target="#navbarNav"
-                      aria-controls="navbarNav"
-                      aria-expanded="false"
-                      aria-label="Toggle navigation"
-                      onClick={this.switch}>
-                <img src={Hamburger} alt="mobile menu"/>
-              </button>
+              <div className="col col-md-1">
+                <a className="navbar-brand" href="#">
+                  <Logo/>
+                </a>
+              </div>
 
-              <div className="navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                  <li className="nav-item active">
-                    <a className="nav-link" href="#">Home <span
-                        className="sr-only">(current)</span></a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="#">About</a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="#">Team</a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="#">Blog</a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="#">Services</a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="#">Contact</a>
-                  </li>
-                </ul>
 
-                <Social/>
+              <div className="col col-md-7 offset-md-4">
+                <div className="navbar-collapse" id="navbarNav">
+                  <ul className="navbar-nav">
+                    <li className="nav-item active">
+                      <a className="nav-link" href="#">Home <span
+                          className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">About</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Team</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Blog</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Services</a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">Contact</a>
+                    </li>
+                  </ul>
+                  <Social/>
+                </div>
+
+                <button className="navbar-toggler"
+                        type="button"
+                        data-toggle="collapse"
+                        data-target="#navbarNav"
+                        aria-controls="navbarNav"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                        onClick={this.switch}>
+                  <img src={Hamburger} alt="mobile menu"/>
+                </button>
 
               </div>
-            </nav>
-          </div>
+
+            </div>
+          </nav>
         </div>
     );
   }
