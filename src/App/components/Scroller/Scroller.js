9070fe994fbc42fd8ab53a0555da683417f692f8
lloan <lloanalas@outlook.com>
2018-12-30 23:49:38 -0800

    set def. props

--- src/App/components/Scroller/Scroller.js
+++ src/App/components/Scroller/Scroller.js
@@ -2,13 +2,19 @@ import React, {Component} from 'react';
 import './Scroller.scss';
 
 class Scroller extends Component {
+  componentWillReceiveProps(nextProps, nextContext) {
+
+  }
 
   render() {
+    const color = this.props.color || 'orange';
+    const layers = this.props.layers || 3;
+
     return (
         <div className="scroller-container min-height-200">
           <div className="scroller mb-5 mt-5">
             {
-              [...Array(this.props.layers)].map(a => <div className={`scrollerElement border-${this.props.color}`} />)
+              [...Array(layers)].map(a => <div className={`scrollerElement border-${color}`} />)
             }
           </div>
         </div>
