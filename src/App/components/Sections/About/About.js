cd6cf0a69aac79dbc26497ba8ada56f11a3620cb
Amber Stumpf <aiyemacbain@gmail.com>
2019-01-03 13:29:24 -0800

    Fixes margins, problem with contianer padding

--- src/App/components/Sections/About/About.js
+++ src/App/components/Sections/About/About.js
@@ -3,6 +3,7 @@ import './About.scss';
 import Scroller from '../../Scroller/Scroller';
 
 
+
 class About extends Component {
     componentWillMount(){
 
@@ -10,50 +11,55 @@ class About extends Component {
 
     render(){
         return(
-          <section className='row about' >
-            <div className='container desktop col-lg-6 m-0' style={{background: '#ef3c24'}}>
-              <p className='section-title big-john py-3 text-white'>NOT YOUR <br />AVERAGE STUDIO</p>
+          <section id='about'>
+          <div className='container p-0' >
+            <div className='row p-0'>
+              <div className='desktop col-lg-6'>
+                <p className='about-title big-john py-4 text-white'>NOT YOUR <br />AVERAGE STUDIO</p>
 
-              <div className='desktop d-none d-lg-block avenir-light text-white'>
-                <p>We take advantage of bleeding edge technology to provide you with the power of an in-house creative team at a fraction of the cost.
+
+                <p className='desktop d-none d-lg-block avenir-light text-white'>We take advantage of bleeding edge technology to provide you with the power of an in-house creative team at a fraction of the cost.
                 </p>
-                <ul className='about-list'>
-                  <li className='about-list-item'><span className='diamond'></span>Exclusively Online</li>
+                <ul className='about-list desktop d-none d-lg-block avenir-light text-white pb-5'>
+                  <li className='about-list-item'><span className='diamond diamond-wh'></span>Exclusively Online</li>
 
-                  <li className='about-list-item'><span className='diamond'></span>Customer Centric</li>
+                  <li className='about-list-item'><span className='diamond diamond-wh'></span>Customer Centric</li>
 
-                  <li className='about-list-item'><span className='diamond'></span>Heavily Invest in Research & Development</li>
+                  <li className='about-list-item'><span className='diamond diamond-wh'></span>Heavily Invest in Research & Development</li>
 
-                  <li className='about-list-item'><span className='diamond'></span>Active Non-profit Supporter</li>
+                  <li className='about-list-item'><span className='diamond diamond-wh'></span>Active Non-profit Supporter</li>
 
-                  <li className='about-list-item'><span className='diamond'></span>Honest & Straightforward</li>
+                  <li className='about-list-item'><span className='diamond diamond-wh'></span>Honest & Straightforward</li>
                 </ul>
+            </div>
+
+
+              <div className='about-content col-lg-6'>
+                <p className='avenir-light pb-5'>Ledkyb Studios is based in the Inland Empire and we operate exclusively
+                online. This allows us to leverage our low overhead to invest in powerful in-house
+                tools. Our team is composed of designers, developers and project managers who are
+                passionate about what they do.
+                </p>
+                <ul className='row about-list d-none d-lg-block'>
+                  <li className='about-list-item'><span className='diamond'></span><a href="../Team/Team">Meet Your Team</a></li>
+
+                  <li className='about-list-item'><span className='diamond'></span><a href="../Services/Services">Services We Offer</a></li>
+
+                  <li className='about-list-item'><span className='diamond'></span><a href="../Blog/Blog">Check Our Blog Out</a></li>
+                </ul>
+
+
+                <div className='art-rocket'>
+
+                </div>
+
               </div>
 
 
-            </div>
-            <div className='container about-content col-lg-6'>
-              <p className='avenir-light'>Ledkyb Studios is based in the Inland Empire and we operate exclusively
-              online. This allows us to leverage our low overhead to invest in powerful in-house
-              tools. Our team is composed of designers, developers and project managers who are
-              passionate about what they do.
-              </p>
-              <ul className='row about-list d-none d-lg-block'>
-                <li className='about-list-item'><span className='diamond orange'></span><a href="#">Meet Your Team</a></li>
 
-                <li className='about-list-item'><span className='diamond orange'></span><a href="#">Services We Offer</a></li>
-
-                <li className='about-list-item'><span className='diamond orange'></span><a href="#">Check Our Blog Out</a></li>
-              </ul>
-            </div>
-
-            <div className='art-rocket'>
 
             </div>
-
-
-            <Scroller/>
-
+          </div>
           </section>
         )
     }
