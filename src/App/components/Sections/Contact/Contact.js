0f85a178578cf5a0a4f12811a12f043ad027c20b
Amy Silva <amyambard@icloud.com>
2019-01-03 11:06:21 -0800

    Adjust header text to match example image

--- src/App/components/Sections/Contact/Contact.js
+++ src/App/components/Sections/Contact/Contact.js
@@ -16,12 +16,12 @@ class Contact extends Component {
 
                   <div className="header row text-center ">
                       <div className="align-self-end col-12 my-md-auto my-lg-auto">
-                          <h1 className="text-uppercase mb-3">
+                          <h1 className="text-uppercase">
                               Start Your Project
                           </h1>
-                          <p>
+                          <span className="headerText">
                               Rather send us an email? Send it to <a href="mailto:support@ledkyb.com?Subject=I%20have%20a%20project">support@ledkyb.com</a>.
-                          </p>
+                          </span>
                       </div>
                   </div>
 
@@ -32,7 +32,7 @@ class Contact extends Component {
                   </div>
 
                   <div className="footer row justify-content-between align-items-md-end">
-                      <div className="col-12 col-lg-4">
+                      <div className="col-12 mb-5 col-lg-4">
                           <SocialMedia />
                       </div>
                       <div className="col-12 col-lg-6 order-lg-first">
