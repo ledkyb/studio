9264570a05c3bbc8a6bbb18550f848406697db35
Amber Stumpf <aiyemacbain@gmail.com>
2019-01-05 13:17:00 -0800

    Fixes typo

--- src/App/components/Sections/About/About.js
+++ src/App/components/Sections/About/About.js
@@ -14,7 +14,7 @@ class About extends Component {
             <div className="container" style={{background: 'pink', height: '150px'}} data-bg="dark" data-section="about">
                 <p>About</p>
             </div>
-          </section>
+          
         )
     }
 }
