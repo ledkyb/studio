5149278118d1fd731d0121d312a75e25487ad59c
Amy Silva <amyambard@icloud.com>
2019-01-07 20:14:59 -0800

    Add alt text to images

--- src/App/components/Sections/Team/ScrollBtn/ScrollBtn.js
+++ src/App/components/Sections/Team/ScrollBtn/ScrollBtn.js
@@ -8,8 +8,8 @@ import arrowBtn from './Assets/leftBlack.png';
 const ScrollBtn = ({ prevBtnStyle, nextBtnStyle, onClickPrev, onClickNext }) => {
   return (
     <div className="team-scroll-btn-container position-absolute d-flex flex-row flex-lg-column justify-content-between align-items-center">
-      <img className="team-scroll-up-btn" style={prevBtnStyle} src={arrowBtn} onClick={onClickPrev} />
-      <img className="team-scroll-down-btn mb-0 mb-lg-5" style={nextBtnStyle} src={arrowBtn} onClick={onClickNext} />
+      <img alt="to previous team members" className="team-scroll-up-btn" style={prevBtnStyle} src={arrowBtn} onClick={onClickPrev} />
+      <img alt="to more team members" className="team-scroll-down-btn mb-0 mb-lg-5" style={nextBtnStyle} src={arrowBtn} onClick={onClickNext} />
     </div>
   );
 };
