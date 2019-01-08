645b68c5ad4f367d17ebe182ca433a4b8bf53824
Amy Silva <amyambard@icloud.com>
2019-01-07 20:09:59 -0800

    Fix, style: change 'class' to 'className'

--- src/App/components/Sections/Blog/Blog.js
+++ src/App/components/Sections/Blog/Blog.js
@@ -11,118 +11,118 @@ class Blog extends Component {
   render(){
     return(
       <div id="blog" className="container" >
-        <div id="carouselExampleControls"  class="carousel  slide " data-ride="carousel">              
-          <div class="carousel-inner">
+        <div id="carouselExampleControls"  className="carousel  slide " data-ride="carousel">
+          <div className="carousel-inner">
 
             {/* slide one */}
-            <div class="carousel-item active">
-              <div class="row">
-                <div class="col-7 blog1 ">                  
-                  <div  class="container postOne ">
-                    <section class="postInner">
-                      <a href="#"  class="postTitle1">POST TITLE, LIMITED TO TWO LINES OF TEXT p1 </a>
-                      <p class="lead authorInfo">John Doe <span class="verticalBar">|</span> Category <span class="verticalBar">|</span> November 22nd,2018 </p>
+            <div className="carousel-item active">
+              <div className="row">
+                <div className="col-7 blog1 ">
+                  <div className="container postOne ">
+                    <section className="postInner">
+                      <a href="#"  className="postTitle1">POST TITLE, LIMITED TO TWO LINES OF TEXT p1 </a>
+                      <p className="lead authorInfo">John Doe <span className="verticalBar">|</span> Category <span className="verticalBar">|</span> November 22nd,2018 </p>
                     </section>
-                  </div>                        
+                  </div>
                 </div>
-                <div class="col-5 sharedBlogSection ">
-                  <div id="blog3" class="col-12 "> 
-                    <div  class="container postTwo ">
-                      <section class="postInner">
-                        <a href="#"  class="postTitle2">POST TITLE, LIMITED TO TWO LINES OF TEXT p2</a>
-                        <p class="lead authorInfo">John Doe <span class="verticalBar">|</span> Category <span class="verticalBar">|</span> November 22nd,2018 </p>
+                <div className="col-5 sharedBlogSection ">
+                  <div id="blog3" className="col-12 ">
+                    <div  className="container postTwo ">
+                      <section className="postInner">
+                        <a href="#"  className="postTitle2">POST TITLE, LIMITED TO TWO LINES OF TEXT p2</a>
+                        <p className="lead authorInfo">John Doe <span className="verticalBar">|</span> Category <span className="verticalBar">|</span> November 22nd,2018 </p>
                       </section>
                     </div>
                   </div>
-                  <div id="blog4" class="col-12 "> 
-                    <div  class="container postThree ">
-                      <section class="postInner">
-                        <a href="#"  class="postTitle3">POST TITLE, LIMITED TO TWO LINES OF TEXT p3</a>
-                        <p class="lead authorInfo">John Doe <span class="verticalBar">|</span> Category <span class="verticalBar">|</span> November 22nd,2018 </p>
+                  <div id="blog4" className="col-12 ">
+                    <div  className="container postThree ">
+                      <section className="postInner">
+                        <a href="#"  className="postTitle3">POST TITLE, LIMITED TO TWO LINES OF TEXT p3</a>
+                        <p className="lead authorInfo">John Doe <span className="verticalBar">|</span> Category <span className="verticalBar">|</span> November 22nd,2018 </p>
                       </section>
                     </div>
-                  </div>                  
-                </div>                        
+                  </div>
+                </div>
               </div>
             </div>
 
 
             {/* silde two */}
-            <div class="carousel-item">
-              <div class="row">
-                <div class="col-7 blog1 ">                               
-                  <div  class="container postOne ">
-                    <section class="postInner">
-                      <a href="#"  class="postTitle1">POST TITLE, LIMITED TO TWO LINES OF TEXT pa</a>
-                      <p class="lead authorInfo">John Doe <span class="verticalBar">|</span> Category <span class="verticalBar">|</span> November 22nd,2018 </p>
+            <div className="carousel-item">
+              <div className="row">
+                <div className="col-7 blog1 ">
+                  <div  className="container postOne ">
+                    <section className="postInner">
+                      <a href="#"  className="postTitle1">POST TITLE, LIMITED TO TWO LINES OF TEXT pa</a>
+                      <p className="lead authorInfo">John Doe <span className="verticalBar">|</span> Category <span className="verticalBar">|</span> November 22nd,2018 </p>
                     </section>
-                  </div>                        
+                  </div>
                 </div>
-                <div class="col-5 sharedBlogSection ">
-                  <div id="blog3" class="col-12 "> 
-                    <div  class="container postTwo ">
-                      <section class="postInner">
-                        <a href="#"  class="postTitle2">POST TITLE, LIMITED TO TWO LINES OF TEXT pb</a>
-                        <p class="lead authorInfo">John Doe <span class="verticalBar">|</span> Category <span class="verticalBar">|</span> November 22nd,2018 </p>
+                <div className="col-5 sharedBlogSection ">
+                  <div id="blog3" className="col-12 ">
+                    <div  className="container postTwo ">
+                      <section className="postInner">
+                        <a href="#"  className="postTitle2">POST TITLE, LIMITED TO TWO LINES OF TEXT pb</a>
+                        <p className="lead authorInfo">John Doe <span className="verticalBar">|</span> Category <span className="verticalBar">|</span> November 22nd,2018 </p>
                       </section>
                     </div>
                   </div>
-                  <div id="blog4" class="col-12 "> 
-                    <div  class="container postThree ">
-                      <section class="postInner">
-                        <a href="#"  class="postTitle3">POST TITLE, LIMITED TO TWO LINES OF TEXT pc</a>
-                        <p class="lead authorInfo">John Doe <span class="verticalBar">|</span> Category <span class="verticalBar">|</span> November 22nd,2018 </p>
+                  <div id="blog4" className="col-12 ">
+                    <div  className="container postThree ">
+                      <section className="postInner">
+                        <a href="#"  className="postTitle3">POST TITLE, LIMITED TO TWO LINES OF TEXT pc</a>
+                        <p className="lead authorInfo">John Doe <span className="verticalBar">|</span> Category <span className="verticalBar">|</span> November 22nd,2018 </p>
                       </section>
                     </div>
-                  </div>                  
-                </div>                        
+                  </div>
+                </div>
               </div>
             </div>
 
             {/* slide three */}
-            <div class="carousel-item">
-              <div class="row">
-                <div class="col-7 blog1 ">                              
-                  <div  class="container postOne ">
-                    <section class="postInner">
-                      <a href="#" class="postTitle1">POST TITLE, LIMITED TO TWO LINES OF TEXT 1</a>
-                      <p class="lead authorInfo">John Doe <span class="verticalBar">|</span> Category <span class="verticalBar">|</span> November 22nd,2018 </p>
+            <div className="carousel-item">
+              <div className="row">
+                <div className="col-7 blog1 ">
+                  <div  className="container postOne ">
+                    <section className="postInner">
+                      <a href="#" className="postTitle1">POST TITLE, LIMITED TO TWO LINES OF TEXT 1</a>
+                      <p className="lead authorInfo">John Doe <span className="verticalBar">|</span> Category <span className="verticalBar">|</span> November 22nd,2018 </p>
                     </section>
-                  </div>                 
+                  </div>
                 </div>
-                <div class="col-5 sharedBlogSection ">
-                  <div id="blog3" class="col-12 "> 
-                    <div  class="container postTwo ">
-                      <section class="postInner">
-                        <a href="#" class="postTitle2">POST TITLE, LIMITED TO TWO LINES OF TEXT 2</a>
-                        <p class="lead authorInfo">John Doe <span class="verticalBar">|</span> Category <span class="verticalBar">|</span> November 22nd,2018 </p>
+                <div className="col-5 sharedBlogSection ">
+                  <div id="blog3" className="col-12 ">
+                    <div  className="container postTwo ">
+                      <section className="postInner">
+                        <a href="#" className="postTitle2">POST TITLE, LIMITED TO TWO LINES OF TEXT 2</a>
+                        <p className="lead authorInfo">John Doe <span className="verticalBar">|</span> Category <span className="verticalBar">|</span> November 22nd,2018 </p>
                       </section>
                     </div>
                   </div>
-                  <div id="blog4" class="col-12 "> 
-                    <div  class="container postThree ">
-                      <section class="postInner">
-                        <a href="#"  class="postTitle3">POST TITLE, LIMITED TO TWO LINES OF TEXT 3</a>
-                        <p class="lead authorInfo">John Doe <span class="verticalBar">|</span> Category <span class="verticalBar">|</span> November 22nd,2018 </p>
+                  <div id="blog4" className="col-12 ">
+                    <div  className="container postThree ">
+                      <section className="postInner">
+                        <a href="#"  className="postTitle3">POST TITLE, LIMITED TO TWO LINES OF TEXT 3</a>
+                        <p className="lead authorInfo">John Doe <span className="verticalBar">|</span> Category <span className="verticalBar">|</span> November 22nd,2018 </p>
                       </section>
                     </div>
-                  </div>                  
-                </div>                        
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-          <a class="carousel-control-prev carosuel" href="#carouselExampleControls" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
+          <a className="carousel-control-prev carosuel" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="sr-only">Previous</span>
           </a>
-          <a class="carousel-control-next carosuel" href="#carouselExampleControls" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
+          <a className="carousel-control-next carosuel" href="#carouselExampleControls" role="button" data-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="sr-only">Next</span>
           </a>
-        </div>                                          
+        </div>
       </div>
-    )      
-  }    
+    )
+  }
 }
 
 
