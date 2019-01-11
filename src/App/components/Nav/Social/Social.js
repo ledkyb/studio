567ff7656b02566c75c690300836542c144b7de2
Amy Silva <amyambard@icloud.com>
2019-01-10 19:46:47 -0800

    Set company name as state for template

--- src/App/components/Nav/Social/Social.js
+++ src/App/components/Nav/Social/Social.js
@@ -8,19 +8,22 @@ import twitter from './Assets/tw.svg';
 import './Social.scss';
 
 class Social extends Component {
+  state = {
+    companyName: 'ledkyb',
+  }
   render(){
     return(
         <div id="socialMenu" className="container-fluid">
           <div className="row">
-            <div className="col-3 col-sm-2 col-md-1 col-lg-1 social-menu-icon" onClick={e => { window.location.href = 'https://www.facebook.com'}}><img src={facebook} alt="facebook social icon"/></div>
-            <div className="col-3 col-sm-2 col-md-1 col-lg-1 social-menu-icon" onClick={e => { window.location.href = 'https://www.github.com'}}><img src={github} alt="github social icon"/></div>
-            <div className="col-3 col-sm-2 col-md-1 col-lg-1 social-menu-icon" onClick={e => { window.location.href = 'https://www.linkedin.com'}}><img src={linkedin} alt="linkedin social icon"/></div>
+            <div className="col-3 col-sm-2 col-md-1 col-lg-1 social-menu-icon" onClick={e => { window.location.href = `https://www.facebook.com/${this.state.companyName}`}}><img src={facebook} alt={`${this.state.companyName}'s facebook page`}/></div>
+            <div className="col-3 col-sm-2 col-md-1 col-lg-1 social-menu-icon" onClick={e => { window.location.href = `https://www.github.com/${this.state.companyName}`}}><img src={github} alt="ledkyb's github page"/></div>
+            <div className="col-3 col-sm-2 col-md-1 col-lg-1 social-menu-icon" onClick={e => { window.location.href = `https://www.linkedin.com/${this.state.companyName}`}}><img src={linkedin} alt={`${this.state.companyName}'s linkedin page`}/></div>
           </div>
 
           <div className="row mt-4">
-            <div className="col-3 col-sm-2 col-md-1 col-lg-1 social-menu-icon" onClick={e => { window.location.href = 'https://www.instagram.com'}}><img src={instagram} alt="instagram social icon"/></div>
-            <div className="col-3 col-sm-2 col-md-1 col-lg-1 social-menu-icon" onClick={e => { window.location.href = 'https://www.twitter.com'}}><img src={twitter} alt="twitter social icon"/></div>
-            <div className="col-3 col-sm-2 col-md-1 col-lg-1 social-menu-icon" onClick={e => { window.location.href = 'https://www.stackoverflow.com'}}><img src={stackoverflow} alt="stackoverflow social icon"/></div>
+            <div className="col-3 col-sm-2 col-md-1 col-lg-1 social-menu-icon" onClick={e => { window.location.href = `https://www.instagram.com/${this.state.companyName}`}}><img src={instagram} alt={`${this.state.companyName}'s instagram page`}/></div>
+            <div className="col-3 col-sm-2 col-md-1 col-lg-1 social-menu-icon" onClick={e => { window.location.href = `https://www.twitter.com/${this.state.companyName}`}}><img src={twitter} alt={`${this.state.companyName}'s twitter page`}/></div>
+            <div className="col-3 col-sm-2 col-md-1 col-lg-1 social-menu-icon" onClick={e => { window.location.href = `https://www.stackoverflow.com/${this.state.companyName}`}}><img src={stackoverflow} alt={`${this.state.companyName}'s stackoverflow page`}/></div>
           </div>
         </div>
     )
