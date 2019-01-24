43153cb718f48a5858c0109bcb1b981e8432868b
tonynguyen111997 <tonynguyen111997@gmail.com>
2019-01-23 22:58:14 -0800

    Connected team section to server

--- src/App/components/Sections/Team/UserInactive/UserList/UserItem/UserItem.js
+++ src/App/components/Sections/Team/UserInactive/UserList/UserItem/UserItem.js
@@ -3,9 +3,10 @@ import React from 'react';
 import './UserItem.scss';
 
 const UserItem = ({ user, loadUser }) => {
-  let { id, name, img, title } = user;
+  let { id, firstName, lastName, role, image } = user;
   return (
-    <div id={id} className="team-user-item">
+    //id
+    <div className="team-user-item">
 
     {/* Item Hovered */}
     <div onClick={() => {loadUser(user)}} className="team-user-item-hover position-absolute justify-content-center align-items-center">
@@ -17,12 +18,12 @@ const UserItem = ({ user, loadUser }) => {
 
       {/* Item */}
       <div onClick={() => {loadUser(user)}} >
-        <img alt="user" src={img} />
+        <img alt="user" src={image} />
         <div className="mb-5 mb-lg-0 d-flex align-items-center py-4">
           <div className="team-user-item-line mx-1 mx-lg-3"></div>
           <div>
-            <p className="font-weight-bold">{name}</p>
-            <p className="team-user-item-title mt-1 mt-lg-0 flex-wrap">{title}</p>
+            <p className="font-weight-bold">{firstName} {lastName}</p>
+            <p className="team-user-item-title mt-1 mt-lg-0 flex-wrap">{role}</p>
           </div>
         </div>
       </div>
--- src/App/components/Sections/Team/fetchTeam.js
+++
@@ -1,8 +0,0 @@
-export const fetchTeam = url => {
-  fetch(url)
-  .then(response => response.json())
-  .then(team => team)
-  .catch(err => {
-    throw new Error("Failed to load team");
-  });
-}
