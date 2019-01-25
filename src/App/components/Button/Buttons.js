0fed71bf106f7ae8c573a3fe226c9812318ba7ae
lloan <lloanalas@outlook.com>
2019-01-24 19:00:57 -0800

    home button update

--- src/App/components/Button/Buttons.js
+++ src/App/components/Button/Buttons.js
@@ -2,10 +2,16 @@ import React, {Component} from 'react';
 import './Button.scss';
 
 class Button extends Component {
-  render() {
-    return (
-        <button className={`button ${this.props.styles}`} href={this.props.link} >{this.props.label}</button>
+  render() { 
+    if (this.props.link !== "") {
+      return (
+        <button className={`button ${this.props.styles}`} onClick={() => {window.location.href = window.location.origin + "/#contact"}} >{this.props.label}</button>
     )
+    } else {
+      return (
+        <button className={`button ${this.props.styles}`} href={this.props.link } >{this.props.label}</button>
+    ) 
+    }
   }
 }
 
